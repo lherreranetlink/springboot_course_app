28167fbc4079c2bdac4c5b93d9f97ef590c1b18e
Leonardo Herrera Ornelas <leonardoherreraornelas960302@gmail.com>
2024-02-02 14:19:58 -0600

    modify schema and remove IngredientRef table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,15 +14,7 @@ create table if not exists Taco_Order (
 create table if not exists Taco (
     id identity,
     name varchar(50) not null,
-    taco_order bigint not null,
-    taco_order_key bigint not null,
-    created_at timestamp not null
-);
-
-create table if not exists Ingredient_Ref (
-    ingredient varchar(4) not null,
-    taco bigint not null,
-    taco_key bigint not null
+   created_at timestamp not null
 );
 
 create table if not exists Ingredient (
@@ -30,8 +22,3 @@ create table if not exists Ingredient (
     name varchar(25) not null,
     type varchar(10) not null
 );
-
-alter table Taco
-add foreign key (taco_order) references Taco_Order(id);
-alter table Ingredient_Ref
-add foreign key (ingredient) references Ingredient(id);
