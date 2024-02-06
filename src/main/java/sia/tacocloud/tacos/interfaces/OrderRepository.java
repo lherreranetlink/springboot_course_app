2319fe3ab5c5189cc5bf15e7d504b8a9f7a51c32
Leonardo Herrera Ornelas <leonardoherreraornelas960302@gmail.com>
2024-02-06 12:11:18 -0600

    added another method to the Order repo

--- src/main/java/sia/tacocloud/tacos/interfaces/OrderRepository.java
+++ src/main/java/sia/tacocloud/tacos/interfaces/OrderRepository.java
@@ -1,7 +1,9 @@
 package sia.tacocloud.tacos.interfaces;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.tacos.domains.TacoOrder;
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
 }
